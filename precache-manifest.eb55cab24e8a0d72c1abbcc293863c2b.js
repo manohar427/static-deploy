4adc1cac3e556e4bbdf7f365f8b83611d6a29224
Manohar S <manohar427@gmail.com>
2020-02-12 00:10:35 +0530

    Updates

---
+++ precache-manifest.eb55cab24e8a0d72c1abbcc293863c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc34b4a1ac4cf9b47a22734214f52736",
+    "url": "/static-deploy/index.html"
+  },
+  {
+    "revision": "1eefaa5305dd4cf16c2b",
+    "url": "/static-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e6d28efbf64bf50b618",
+    "url": "/static-deploy/static/js/2.cee4650a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static-deploy/static/js/2.cee4650a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eefaa5305dd4cf16c2b",
+    "url": "/static-deploy/static/js/main.4fbd93fb.chunk.js"
+  },
+  {
+    "revision": "ab0e7be973da35e85744",
+    "url": "/static-deploy/static/js/runtime-main.7ba74f7f.js"
+  }
+]);
